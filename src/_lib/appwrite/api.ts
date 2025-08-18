a7f4c059ad8749edf8b6ea5e23bb3bed01e422db
khan632 <rmustafa632@gmail.com>
2025-08-18 23:26:07 +0530

    Support signup funtionality using appwrite

--- src/_lib/appwrite/api.ts
+++ src/_lib/appwrite/api.ts
@@ -3,14 +3,16 @@ import { account } from "./config";
 import { ID } from "appwrite";
 
 export async function createNewUserAccount(user: NewUserType) {
+    console.log(ID.unique(), "data");
+    
     try {
-        const newUser = await account.create(
+        const newAccount = await account.create(
             ID.unique(),
             user.email,
             user.password,
-            user.name,
+            user.name
         )
-        return newUser;    
+        return newAccount;    
     } catch (error) {
         console.log(error);
         return error;
